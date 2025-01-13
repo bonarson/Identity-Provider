bf7f90946631527a8a2cffa0a4147367d72c9dc4
bonarson <hei.bonarson@gmail.com>
2025-01-13 17:39:17 +0300

    fix:api_key

--- pages/Login.tsx
+++ pages/Login.tsx
@@ -5,6 +5,7 @@ import styles from './style.module.css'; // Importation du module CSS
 
 // Configuration Firebase
 const firebaseConfig = {
+  
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
